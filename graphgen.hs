b110c81b39dcda5941fafabff5f00bca24176c0c
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-27 15:38:32 -0700

    Added complete graph generator

--- graphgen.hs
+++ graphgen.hs
@@ -10,5 +10,5 @@ import Graph (graphFromEdgeList)
 list n = graphFromEdgeList []
 cycle' n = graphFromEdgeList []
 star n = graphFromEdgeList []
-complete n = graphFromEdgeList []
+complete n = graphFromEdgeList [((show a), (show b)) | a <- [1..n], b <- [1..n], a /= b]
 binaryTree n = graphFromEdgeList []
