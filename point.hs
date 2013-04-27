c5a6324a917e273155d99b2b030582189793358b
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-27 16:38:36 -0700

    Added Point.sum

--- point.hs
+++ point.hs
@@ -3,7 +3,8 @@ module Point (
     min',
     add,
     sub,
-    mul_by_constant
+    mul_by_constant,
+    Point.sum,
 ) where
 
 normal (x,y) = sqrt (x^2 + y^2)
@@ -15,3 +16,6 @@ min' (ax,ay) (bx,by) | ax < bx = (ax,ay)
 add (ax,ay) (bx,by) = (ax+bx, ay+by)
 sub (ax,ay) (bx,by) = (ax-bx, ay-by)
 mul_by_constant (x,y) c = (c * x, c * y)
+
+{--sum = foldl Point.add (0,0)--}
+sum ps = (Prelude.sum (map fst ps), Prelude.sum (map snd ps))
