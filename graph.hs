4fbd4346f8758d2967913e4b5140f84c47e661d5
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-27 12:52:23 -0700

    Added read graph file fn

--- graph.hs
+++ graph.hs
@@ -1,4 +1,6 @@
 import Data.Graph
+import System.IO
+import Data.List.Split
 
 newGraph p = buildG (0, p) []
 
@@ -6,3 +8,11 @@ addEdge g v1 v2 = buildG (0, p) ((v1,v2):(v2,v1):(edges g))
     where p = length (vertices g)
 
 getEdges = edges 
+
+
+readGraphFile path = do
+    contents <- readFile path
+    let lines = endBy "\n" contents
+    let p = read (head (tail (splitOn " " (head lines)))) :: Int
+    let edges_lines = tail lines
+    return (p, edges_lines)
