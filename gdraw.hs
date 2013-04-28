e9bd6bedfeafa9957abacdd8928757a470d133f7
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-28 09:29:47 -0700

    Implemented positionNodes

--- gdraw.hs
+++ gdraw.hs
@@ -1,6 +1,7 @@
 module GDraw (
     repulsiveForce,
     attractiveForce,
+    positionNodes,
 ) where
 import qualified Data.Graph as Graph
 import qualified Data.Map as Map
@@ -22,15 +23,15 @@ fr :: Floating a => Graph.Graph -> Int -> Int -> a -> a
 fr g w l z = (k^2) / z
     where k = kfun g w l
 
+disp' f vpos upos = Point.scale delta ((f norm) / norm)
+  where delta = Point.sub vpos upos
+        norm = max (Point.normal delta) 0.01
+
 repulsiveForce :: (Ord a, Floating a) => Graph.Graph -> [(a,a)] -> Int -> Int -> [(a,a)]
 repulsiveForce g ps w l = map disp vs
     where vs = zip (Graph.vertices g) ps
           disp (v,vpos) = Point.sum [disp' f vpos upos | (u,upos) <- vs, u /= v]
           f norm = fr g w l norm
-
-disp' f vpos upos = Point.mul_by_constant delta ((f norm) / norm)
-  where delta = Point.sub vpos upos
-        norm = max (Point.normal delta) 0.01
 
 edgeMap g = Map.fromListWith (++) edge_list
     where edge_list = map (\(a,b) -> (a,[b])) (Graph.edges g)
@@ -50,3 +51,11 @@ attractiveForce g ps w l = map disp vs
           disp v = Point.sub (sumv adjacentTo v) (sumv adjacentFrom v)
           f norm = fa g w l norm
           sumv fn (v,vpos) = Point.sum [disp' f vpos upos | (u,upos) <- fn v]
+
+positionNodes :: (Ord a, Floating a) => Graph.Graph -> [(a,a)] -> [(a,a)] -> [(a,a)] -> Int -> Int -> a -> [(a,a)]
+positionNodes g pos rdisp adisp w l temp = zipWith3 repo pos rdisp adisp
+    where repo vpos r a = fitInCanvas (Point.add vpos (Point.scale dispv (temp/(Point.normal dispv))))
+            where dispv = Point.add r a
+          fitInCanvas (x,y) = (min (w'/2) (max (-w'/2) x), min (l'/2) (max (-l'/2) y))
+            where w' = fromIntegral w
+                  l' = fromIntegral l
