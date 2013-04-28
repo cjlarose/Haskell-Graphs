45e706538cd9a2eeb714ff7e6c1975e5d03a8aa9
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-27 22:12:25 -0700

    Added fa fn

--- gdraw.hs
+++ gdraw.hs
@@ -4,6 +4,18 @@ module GDraw (
 import qualified Data.Graph as Graph
 import qualified Point
 
+{--kfun :: Floating a => Graph.Graph -> Int -> Int -> a--}
+kfun g w l = (sqrt (x/y)) * 5
+    where x = fromIntegral (w*l)
+          y = fromIntegral (length (Graph.vertices g))
+
+fa g w l z = (z^2) / k
+    where k = kfun g w l
+
+{--fr :: Floating a => Graph.Graph -> Int -> Int -> a -> a--}
+fr g w l z = (k^2) / z
+    where k = kfun g w l
+
 {--repulsiveForce :: (Ord a, Floating a) => Graph.Graph -> [(a,a)] -> Int -> Int -> [(a,a)]--}
 repulsiveForce g ps w l = map disp vs
     where vs = zip (Graph.vertices g) ps
@@ -12,12 +24,3 @@ repulsiveForce g ps w l = map disp vs
               where delta = Point.sub vpos upos
                     norm = max (Point.normal delta) 0.01
                     f = fr g w l norm
-
-{--kfun :: Floating a => Graph.Graph -> Int -> Int -> a--}
-kfun g w l = (sqrt (x/y)) * 5
-    where x = fromIntegral (w*l)
-          y = fromIntegral (length (Graph.vertices g))
-
-{--fr :: Floating a => Graph.Graph -> Int -> Int -> a -> a--}
-fr g w l z = (k^2) / z
-    where k = kfun g w l
