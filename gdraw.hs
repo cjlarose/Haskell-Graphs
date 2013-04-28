2df8554220d2f1132d0b423536061f8df74fcb32
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-27 22:09:13 -0700

    Added max norm 0.01 check

--- gdraw.hs
+++ gdraw.hs
@@ -4,20 +4,20 @@ module GDraw (
 import qualified Data.Graph as Graph
 import qualified Point
 
-repulsiveForce :: Floating a => Graph.Graph -> [(a,a)] -> Int -> Int -> [(a,a)]
+{--repulsiveForce :: (Ord a, Floating a) => Graph.Graph -> [(a,a)] -> Int -> Int -> [(a,a)]--}
 repulsiveForce g ps w l = map disp vs
     where vs = zip (Graph.vertices g) ps
           disp (v,vpos) = Point.sum [disp' (v,vpos) (u,upos) | (u,upos) <- vs, u /= v]
           disp' (v,vpos) (u,upos) = Point.mul_by_constant delta (f / norm)
               where delta = Point.sub vpos upos
-                    norm = Point.normal delta
+                    norm = max (Point.normal delta) 0.01
                     f = fr g w l norm
 
-kfun :: Floating a => Graph.Graph -> Int -> Int -> a
+{--kfun :: Floating a => Graph.Graph -> Int -> Int -> a--}
 kfun g w l = (sqrt (x/y)) * 5
     where x = fromIntegral (w*l)
           y = fromIntegral (length (Graph.vertices g))
 
-fr :: Floating a => Graph.Graph -> Int -> Int -> a -> a
+{--fr :: Floating a => Graph.Graph -> Int -> Int -> a -> a--}
 fr g w l z = (k^2) / z
     where k = kfun g w l
