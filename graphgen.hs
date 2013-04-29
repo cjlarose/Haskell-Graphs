96ac91af3f9f9172d012129a57c323078584f4c6
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-28 18:11:49 -0700

    Added list, cycle, star to graphgen

--- graphgen.hs
+++ graphgen.hs
@@ -1,15 +1,19 @@
 module GraphGen (
     list,
-    cycle',
+    GraphGen.cycle,
     star,
     complete,
-    binaryTree
+    binaryTree,
 ) where
 import Graph (graphFromEdgeList)
 
-list n = graphFromEdgeList []
-cycle' n = graphFromEdgeList []
-star n = graphFromEdgeList []
+list n = graphFromEdgeList [(show v, show (v+1)) | v <- [1..(n-1)]]
+
+cycle n = graphFromEdgeList [(show v, show ((v+1) `mod` n)) | v <- [1..n]]
+
+star n = graphFromEdgeList [("1", (show v)) | v <- [2..n]]
+
 complete n = graphFromEdgeList 
     [((show a), (show b)) | a <- [1..n], b <- [1..n], a /= b]
+
 binaryTree n = graphFromEdgeList []
