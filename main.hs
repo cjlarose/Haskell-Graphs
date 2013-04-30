048d8ccdbdd04c0d896ccf029fefa982c8cedcd7
Roey Chasman <rchasman@email.arizona.edu>
2013-04-29 18:32:15 -0700

    Added some restrictions, worked out a ton of bugs but above all got the thing drawing

--- main.hs
+++ main.hs
@@ -4,6 +4,7 @@ import System.Exit
 import System.IO
 import Data.Map (fromList)
 import Data.List
+import Data.Graph
 import Data.Char
 import Data.Maybe (fromMaybe)
 import Control.Monad
@@ -11,97 +12,176 @@ import Text.Printf
 import qualified Draw
 import qualified GraphGen
 
---main = getArgs >>= parse
+-- -- -- -- -- -- -- -- -- --
+--   Command Line Parsing  --
+-- -- -- -- -- -- -- -- -- --
 main = do
-        (flags, files) <- getArgs >>= parse
-        putStrLn $ "Flags: " ++ show flags
-        putStrLn $ "Files: " ++ show files
-        let files = if null files then ["-"] else files
-        if help flags
-            then do hPutStrLn stderr (usageInfo header options)
-                    exitWith ExitSuccess
-            else return (nub [flags], files)
+    (flags, files) <- getArgs >>= parse
+    -- DEBUG
+    putStrLn $ "Flags: " ++ show flags
+    putStrLn $ "Files: " ++ show files
+    -- Help Flag
+    if help flags
+        then do hPutStrLn stderr usage
+                exitWith ExitSuccess
+        else return (nub [flags], files)
+    -- Tweak Flag
+    if (tweak flags) /= (tweak defaultOptions)
+        then do putStrLn $ "TWEAKIGN " ++ show (tweak flags)
+        else putStrLn $ "NOT TWEAKIGN"
+    -- Delay Flag
+    if (delay flags) /= (delay defaultOptions)
+        then do putStrLn $ "DELAY " ++ show (delay flags)
+        else putStrLn $ "NOT DELAY"
+    -- Iterations Flag
+    if (iterations flags) /= (iterations defaultOptions)
+        then do putStrLn $ "IRETATING " ++ show (iterations flags)
+        else putStrLn $ "NOT ITEARTING"
+    -- Width Flag
+    if (width flags) /= (width defaultOptions)
+        then do putStrLn $ "WIDENING " ++ show (width flags)
+        else putStrLn $ "NOT WIDENING"
+    -- Height Flag
+    if (height flags) /= (height defaultOptions)
+        then do putStrLn $ "HEIGHING " ++ show (height flags)
+        else putStrLn $ "NOT HEIGHING"
+    -- Nodes Flag
+    if (nodes flags) /= (nodes defaultOptions)
+        then do putStrLn $ "NODES++ " ++ show (nodes flags)
+        else putStrLn $ "NOT NODES"
+    -- Graph Type Flag
+    let gt = elemIndex (graph flags) graphTypes
+    let (g,_,_) = defineGraph (graphTypes !! fromMaybe 0 gt) (nodes flags)
+    -- File Flag
+    if (file flags) /= (file defaultOptions)
+        then do putStrLn $ "file " ++ show (file flags)
+        else putStrLn $ "NOT file"
+    -- Social File Flag
+    if (socFile flags) /= (socFile defaultOptions)
+        then do putStrLn $ "SOCfile " ++ show (socFile flags)
+        else putStrLn $ "NOT SOCfile"
+    Draw.createWindow g (width flags) (height flags) (iterations flags) (tweak flags)
 
-env = Data.Map.fromList `fmap` getEnvironment
+parse argv = case getOpt Permute options argv of
+    (args,fs,[]) -> return (foldl (flip id) defaultOptions args, fs)
+    (_,_,errs) -> blowUp errs
 
-header = concat ["\nExample usage:\n",
-                 "\t\t./main -g tree -n 10 -i 50 -d 0\n\n",
-                 "Available Options:"]
+blowUp errs = ioError (userError (concat errs ++ usage))
+
+defineGraph graph nodes
+    | graph == "star" = GraphGen.star nodes
+    | graph == "cycle" = GraphGen.cycle nodes
+    | graph == "tree" = GraphGen.binaryTree nodes
+    | graph == "complete" = GraphGen.complete nodes
+    | otherwise = GraphGen.list nodes
+
+-- -- -- -- -- -- -- -- -- --
+--    Utility Functions    --
+-- -- -- -- -- -- -- -- -- --
+
+usage = usageInfo (concat header) options
+
+header = ["\nExample usage:\n",
+          "\t\t./main -g tree -n 10 -i 50 -d 0\n\n",
+          "Available Options:"]
 
 graphTypes = ["list", "cycle", "star", "complete", "tree"]
 
+-- -- -- -- -- -- -- -- -- --
+--    Option Definitions   --
+-- -- -- -- -- -- -- -- -- --
+
 data Options = Options
-    { tweak       :: Int ,
-      delay       :: Int ,
-      iterations  :: Int ,
-      width       :: Int ,
-      height      :: Int ,
-      nodes       :: Int ,
-      file        :: Maybe FilePath ,
-      socFile     :: Maybe FilePath ,
-      graph       :: String ,
+    { tweak       :: Float,
+      delay       :: Int,
+      iterations  :: Int,
+      width       :: Int,
+      height      :: Int,
+      nodes       :: Int,
+      file        :: Maybe FilePath,
+      socFile     :: Maybe FilePath,
+      graph       :: String,
       help        :: Bool
-    } deriving (Show, Eq)
+    } deriving (Show, Eq, Ord)
 
 defaultOptions = Options
-    { tweak        = 0 ,
-      delay        = 100 ,
-      iterations   = 20 ,
-      width        = 500 ,
-      height       = 500 ,
-      nodes        = 5 ,
-      file         = Nothing ,
-      socFile      = Nothing ,
-      graph        = "List" ,
+    { tweak        = 0.0,
+      delay        = 100,
+      iterations   = 20,
+      width        = 500,
+      height       = 500,
+      nodes        = 5,
+      file         = Nothing,
+      socFile      = Nothing,
+      graph        = head graphTypes,
       help         = False
     }
 
 options :: [OptDescr (Options -> Options)]
-options =
-        [Option ['C'] [] (ReqArg (\x o -> o {tweak = read x}) "<TWEAK>")
-            "Tweak factor. [0..1]",
-         Option ['d'] [] (ReqArg (\x o -> o {delay = read x}) "<DELAY>")
-            "Delay in milliseconds between graphs. Default is 100.",
-         Option ['i'] [] (ReqArg (\x o -> o {iterations = read x}) "<#ITERATIONS>")
-            "Number of iterations. Default is 20.",
-         Option ['W'] [] (ReqArg (\x o -> o {width = read x}) "<WINDOW WIDTH>")
-            "Window width. Defualt is 500.",
-         Option ['H'] [] (ReqArg (\x o -> o {height = read x}) "<WINDOW HEIGHT>")
-            "Window height. Default is 500.",
-         Option ['n'] [] (ReqArg (\x o -> o {nodes = read x}) "<#NODES>")
-            "Number of nodes in the graph. Default is 5.",
-         Option ['f'] [] (ReqArg (\x o -> o {file = Just x}) "<FILENAME>")
-            "Specify a file to load a graph from.",
-         Option ['s'] [] (ReqArg (\x o -> o {socFile = Just x}) "<FILENAME>")
-            "Specify a file to load a social graph from.",
-         Option ['g'] [] (ReqArg (\x o -> o {graph =  x}) "<TYPE>")
-            "Choose a graph type. Default is linked list.",
-         Option ['h'] [] (NoArg (\o -> o {help = True}))
-            "Print help and exit."
-        ]
-
-
--- -- -- -- -- -- -- -- -- --
---   Command Line Parsing  --
--- -- -- -- -- -- -- -- -- --
-
-parse argv = case getOpt Permute options argv of
-    (args,fs,[]) -> return (foldl (flip id) defaultOptions args, fs)
-    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
-
---   do
---       let files = if null fs then ["-"] else fs
---       if Help `elem` args then
---           do hPutStrLn stderr (usageInfo header flags)
---              >> exitWith ExitSuccess
---       else
---           Draw.createWindow graph 640 480 20 0
---
---   (_,_,errs) -> do
---Define our graph for later usage
-(g,_ ,_) = GraphGen.star 7
-
---parse ["-g"] = do params <- sequence []
+options = [Option ['h'] []
+               (NoArg (\o -> o {help = True}))
+               "Print help and exit.",
+           Option ['C'] []
+               (ReqArg (\x o -> o {tweak = read x})
+                                    "<TWEAK>")
+               "Sets the tweak factor. Between [0..1]. (0.0 Default)",
+           Option ['d'] []
+               (ReqArg (\x o -> o {delay =
+                                    if read x >= 0 && read x <= 10000000
+                                        then read x
+                                        else delay defaultOptions})
+                                    "<DELAY>")
+               "Delay in milliseconds between graphs. (100 Default)",
+           Option ['i'] []
+               (ReqArg (\x o -> o {iterations =
+                                    if read x >= 0 && read x <= 1000
+                                        then read x
+                                        else iterations defaultOptions})
+                                    "<#ITERATIONS>")
+               "Number of iterations. (20 Default)",
+           Option ['W'] []
+               (ReqArg (\x o -> o {width =
+                                    if read x >= 100 && read x <= 3000
+                                        then read x
+                                        else width defaultOptions})
+                                    "<WIDTH>")
+               "Window width. (500 Default)",
+           Option ['H'] []
+               (ReqArg (\x o -> o {height =
+                                    if read x >= 100 && read x <= 2500
+                                        then read x
+                                        else height defaultOptions})
+                                    "<HEIGHT>")
+               "Window height. (500 Default).",
+           Option ['n'] []
+               (ReqArg (\x o -> o {nodes =
+                                    if read x >= 1 && read x <= 3000
+                                        then read x
+                                        else nodes defaultOptions})
+                                    "<#NODES>")
+               "Number of nodes in the graph. (5 Default).",
+           Option ['f'] []
+               (ReqArg (\x o -> o {file =
+                                    if length x > 0
+                                        then Just x
+                                        else file defaultOptions})
+                                    "<FILENAME>")
+               "Specify a file to load a graph from.",
+           Option ['s'] []
+               (ReqArg (\x o -> o {socFile =
+                                    if length x > 0
+                                        then Just x
+                                        else socFile defaultOptions})
+                                    "<FILENAME>")
+               "Specify a file to load a social graph from.",
+           Option ['g'] []
+               (ReqArg (\x o -> o {graph =
+                                    if x `elem` graphTypes
+                                        then x
+                                        else graph defaultOptions})
+                                    "<TYPE>")
+               (unlines $
+                    ["Choose a graph type: (list Default)"] ++ graphTypes)]
 
 -- -- -- -- -- -- -- -- -- --
 --     Exit Definitions    --
