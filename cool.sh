4fa4a6f1c149db5e6488b319caf5c76d497045cd
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-09-26 19:26:04 -0700

    Added cool thing

--- cool.sh
+++ cool.sh
@@ -1,2 +1,3 @@
 ./main -g complete -d 30 -n 40 -i 120 --edgeColor Magenta --nodeColor Black -C 30
 ./main -g star -d 30 -n 20 -i 120 --edgeColor Green --nodeColor Black
+./main -g cycle -d 100 -n 40 -i 120 --edgeColor Magenta --nodeColor Black -C 30
